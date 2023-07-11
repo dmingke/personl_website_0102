8c6728329fb8e87f68e57bc27db302e2d72d695e
dmingke <73776136+dmingke@users.noreply.github.com>
2023-07-11 00:49:24 -0400

    resume list

--- src/screens/Resume/Resume.jsx
+++ src/screens/Resume/Resume.jsx
@@ -35,22 +35,18 @@ export const Resume = () => {
                   <br />
                 </span>
               </div>
-              <div className="span-wrapper">
-                <span className="text-wrapper-6">
-                  Bachelor of Science in Psychology &amp; Cognitive
-                  Science                                                                               
-                  <br />
-                </span>
-              </div>
-              <div className="text-3">
-                <span className="text-wrapper-6">
-                  Minor in Computer Science and UX Design
-                  <br />
-                </span>
-              </div>
-              <div className="text-3">
-                <span className="text-wrapper-6">Major GPA: 3.87/4.0; Cumulative GPA: 3.6/4.0</span>
-              </div>
+              <ul>
+                <li className="span-wrapper">
+                    Bachelor of Science in Psychology &amp; Cognitive
+                    Science                                                                               
+                </li>
+                <li className="text-3">
+                    Minor in Computer Science and UX Design
+                </li>
+                <li className="text-3">
+                  Major GPA: 3.87/4.0; Cumulative GPA: 3.6/4.0
+                </li>
+              </ul>
             </div>
           </div>
           <div className="group-3">
@@ -79,53 +75,35 @@ export const Resume = () => {
                     <br />
                   </span>
                 </div>
-                <div className="text-4">
-                  <span className="text-wrapper-6">
-                    Led a 10-person team conducting user needs analysis via card sorting, usability tests, Google Form
-                    surveys, and interviews with over 300 Wolverine Access website users.
-                    <br />
-                  </span>
-                </div>
-                <div className="text-4">
-                  <span className="text-wrapper-6">
-                    Acquired user feedback from 100+ participants on self-developed 1cademy.com through surveys and
-                    contextual inquiry, and other UX methods.
-                    <br />
-                  </span>
-                </div>
-                <div className="text-5">
-                  <span className="text-wrapper-6">
-                    Analyzed qualitative data collected mainly using affinity diagramming via Miro.com.
-                    <br />
-                  </span>
-                </div>
-                <div className="text-4">
-                  <span className="text-wrapper-6">
-                    Designed low-fidelity wireframe, identity guide, user flow, storyboard, style guide, and website
-                    prototype, on Figma/Sketch, Balsamig, Adobe Photoshop, and Adobe Illustrator.
-                    <br />
-                  </span>
-                </div>
-                <div className="text-4">
-                  <span className="text-wrapper-6">
-                    Collaborated with developing team to construct 1cademy.com using HTML5, CSS3, Flask, and React based
-                    on established prototypes and style guides.
-                    <br />
-                  </span>
-                </div>
-                <div className="text-4">
-                  <span className="text-wrapper-6">
-                    Employed heuristic principles for system evaluations on 1cademy, incorporating heuristic evaluation,
-                    cognitive walkthroughs, and competitive analysis.
-                    <br />
-                  </span>
-                </div>
-                <div className="text-4">
-                  <span className="text-wrapper-6">
-                    Enhanced usability of 1cademy and Wolverine Access, evidenced by a statistically significant 14%
-                    decrease in user time to locate specific information, as confirmed by usability tests.
-                  </span>
-                </div>
+                <ul>
+                  <li className="text-4">
+                      Led a 10-person team conducting user needs analysis via card sorting, usability tests, Google Form
+                      surveys, and interviews with over 300 Wolverine Access website users.
+                  </li>
+                  <li className="text-4">
+                      Acquired user feedback from 100+ participants on self-developed 1cademy.com through surveys and
+                      contextual inquiry, and other UX methods.
+                  </li>
+                  <li className="text-5">
+                      Analyzed qualitative data collected mainly using affinity diagramming via Miro.com.
+                  </li>
+                  <li className="text-4">
+                      Designed low-fidelity wireframe, identity guide, user flow, storyboard, style guide, and website
+                      prototype, on Figma/Sketch, Balsamig, Adobe Photoshop, and Adobe Illustrator.
+                  </li>
+                  <li className="text-4">
+                      Collaborated with developing team to construct 1cademy.com using HTML5, CSS3, Flask, and React based
+                      on established prototypes and style guides.
+                  </li>
+                  <li className="text-4">
+                      Employed heuristic principles for system evaluations on 1cademy, incorporating heuristic evaluation,
+                      cognitive walkthroughs, and competitive analysis.
+                  </li>
+                  <li className="text-4">
+                      Enhanced usability of 1cademy and Wolverine Access, evidenced by a statistically significant 14%
+                      decrease in user time to locate specific information, as confirmed by usability tests.
+                  </li>
+                </ul>
               </div>
             </div>
             <div className="flex-container-4">
@@ -149,40 +127,28 @@ export const Resume = () => {
                   <br />
                 </span>
               </div>
-              <div className="text-6">
-                <span className="text-wrapper-6">
-                  Analyzed research data with SPSS and R, produced over 4 visual reports collaborating with project
-                  leads.
-                  <br />
-                </span>
-              </div>
-              <div className="text-6">
-                <span className="text-wrapper-6">
-                  Assisted project leads in crafting 40+ regulatory documents and presentations to highlight research
-                  findings.
-                  <br />
-                </span>
-              </div>
-              <div className="text-6">
-                <span className="text-wrapper-6">
-                  Co-developed experimental websites and Qualtrics questionnaires with 3 team leads, gathering data from
-                  200+ MTurk participants.
-                  <br />
-                </span>
-              </div>
-              <div className="text-6">
-                <span className="text-wrapper-6">
-                  Ensured data integrity by meticulously auditing incoming data for accuracy, identifying, and
-                  rectifying issues.
-                  <br />
-                </span>
-              </div>
-              <div className="text-6">
-                <span className="text-wrapper-6">
-                  Presented research results at the Undergraduate Research Festival, effectively communicating findings
-                  to a broad audience.
-                </span>
-              </div>
+              <ul>
+                <li className="text-6">
+                    Analyzed research data with SPSS and R, produced over 4 visual reports collaborating with project
+                    leads.
+                </li>
+                <li className="text-6">
+                    Assisted project leads in crafting 40+ regulatory documents and presentations to highlight research
+                    findings.
+                </li>
+                <li className="text-6">
+                    Co-developed experimental websites and Qualtrics questionnaires with 3 team leads, gathering data from
+                    200+ MTurk participants.
+                </li>
+                <li className="text-6">
+                    Ensured data integrity by meticulously auditing incoming data for accuracy, identifying, and
+                    rectifying issues.
+                </li>
+                <li className="text-6">
+                    Presented research results at the Undergraduate Research Festival, effectively communicating findings
+                    to a broad audience.
+                </li>
+              </ul>
             </div>
           </div>
           <div className="flex-container-5">
@@ -192,34 +158,22 @@ export const Resume = () => {
                 <br />
               </span>
             </div>
-            <div className="text-7">
-              <span className="text-wrapper-5">
+            <ul className="text-7">
+              <li className="text-wrapper-5">
                 First Generation Honors Association. Event Coordinator, Member
-                <br />
-              </span>
-            </div>
-            <div className="text-7">
-              <span className="text-wrapper-5">
+              </li>
+              <li className="text-wrapper-5">
                 UM_CSSA, Minister of Propaganda Department. 
-                <br />
-              </span>
-            </div>
-            <div className="text-7">
-              <span className="text-wrapper-5">
+              </li>
+              <li className="text-wrapper-5">
                 Mandarin, English, Japanese, Fluent
-                <br />
-              </span>
-            </div>
-            <div className="text-8">
-              <span className="text-wrapper-5">
+              </li>
+              <li className="text-wrapper-5">
                 Python, CSS3, HTML5, JavaScript, React, C++, SPSS, R, Photoshop, Illustrator, Figma, Shell scripting,
                 Git, familiar
-                <br />
-              </span>
-            </div>
-            <div className="text-7">
-              <span className="text-wrapper-5">Around 10000 lines coding experience</span>
-            </div>
+              </li>
+              <li className="text-wrapper-5">Around 10000 lines coding experience</li>
+            </ul>
           </div>
         </div>
       </div>
